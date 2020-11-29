56b1320700d96b1d8c8da5fdc6def9cc1b19f007
akash.chatterjee <akash.chatterjee@bobble.ai>
2020-11-29 22:17:04 +0530

    Fixed unused variable issue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main';
-import dotenv from 'dotenv'
-let result = dotenv.config('.env')
 ReactDOM.render(
   <Main />,
   document.getElementById('root')
